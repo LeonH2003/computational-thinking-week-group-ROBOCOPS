276d453ba8d2771ded1db0d5c055f9f4beb66b6f
LeonH2003 <leon.heemskerk@student.uva.nl>
2025-09-01 10:31:39 +0200

    made a short story for the bonus

--- challenge_day3/leon.py
+++ challenge_day3/leon.py
@@ -1,2 +1,13 @@
 def name():
     return "Leon"
+
+def act1():
+    return "Hi, I am Leon a knight travelling to save the princess from a dragon"
+
+
+
+def act2():
+    return "I find the dragon and challenge it to duel"
+
+def act3():
+    return "I am burned alive. Like a roasted marshmellow by that stupid dragon"

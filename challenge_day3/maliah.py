6c2bf6e096e89eb77f357581da9608f846753140
grapel2 <hannikaisa@gmail.com>
2025-09-01 12:06:26 +0300

    maliah fix

--- challenge_day3/maliah.py
+++ challenge_day3/maliah.py
@@ -1,8 +1,8 @@
 def name():
     return "Maliah"
 def act1():
-    return "Hello, I'm Maliah, I am a fruit bat. I like fruits and sleeping. I sleep in fruit trees that are at least two meters tall. My favourite fruit is raspberry. I also am living in the forest with the others, the siren, witch, mermaid, dragon, and knight "
+    return "Hello, I'm Maliah, I am a fruit bat. I like fruits and sleeping. I sleep in fruit trees that are at least two meters tall. My favourite fruit is raspberry. I love swimming to the Baltic sea every morning. I watch my neigbors all day and I whisper spells that catch their ears in my sleep. I often get myself in sticky situations because of this; something about an electric eel. I neighbor with the witch and its stupid pet dragon. sometimes lost knights stumble through our forest too... "
 def act2():
-    return "Today I, the fruitbat, was putting my ear against the ocean and I heard a lot of electricity in the ocean... eek. I could not conclude anything from this so I flew home. I heard from the other friut bats that the siren is now an electric eel... how curious..." 
+    return "Today I, the fruitbat, was putting my ear against the sea and I heard a lot of electricity in the sea... eek!! I soon found this group of magnificent electric eels and followed them around as they soared through the water. They were showing me all the colors of the rainbow! I heard from the other fruit bats that they had seen a siren earlier that day how curious..."
 def act3():
-    return "I, the fruitbat, think that maybe all fruit bats should evolve to be in the ocean and grow water parts so they can be friends with the siren and the mermaid. We shall see."
+    return "I, the fruitbat, think that maybe all fruit bats should evolve to be in the ocean and grow water parts so they can be friends with the sirens and their mind bending abilities and the mermaids in touch with the cycles of the moooooon. We shall see."

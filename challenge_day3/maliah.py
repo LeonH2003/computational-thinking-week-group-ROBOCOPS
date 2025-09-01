46a09600f1deb5cc05393d737aa841bf11489b31
klaudialegaspi <klaudia.legaspi@student.uva.nl>
2025-09-01 10:47:25 +0200

    i spell checked maliah.py

--- challenge_day3/maliah.py
+++ challenge_day3/maliah.py
@@ -1,8 +1,8 @@
 def name():
     return "Maliah"
 def act1():
-    return "Helo, I'm Maliah, I am a fruit bat. I liek fruits and sleeping. I sleep in friut trees that are at least too meters tall. My faverit furit is raspbery. I also am living in teh forest with the others, the syren, wicth, mermaid, dragon, and knight "
+    return "Hello, I'm Maliah, I am a fruit bat. I like fruits and sleeping. I sleep in fruit trees that are at least two meters tall. My favourite fruit is raspberry. I also am living in the forest with the others, the siren, witch, mermaid, dragon, and knight "
 def act2():
-    return "Today i, the fruitbat, was putting my ear against the ocean and i heard a lot of electricity in the ocean... eek. i could not conclued anyting from this so i flew home. i heard from the other friut bats thta siren is now electric eel... how curious..." 
+    return "Today I, the fruitbat, was putting my ear against the ocean and I heard a lot of electricity in the ocean... eek. I could not conclude anything from this so I flew home. I heard from the other friut bats that the siren is now an electric eel... how curious..." 
 def act3():
-    return "I, the friutbat, think that maybe all fruit bats should evolve to be in the ocena and grow water parts so they can be freinds with the siren and the mermaid. We shall see."
+    return "I, the fruitbat, think that maybe all fruit bats should evolve to be in the ocean and grow water parts so they can be friends with the siren and the mermaid. We shall see."

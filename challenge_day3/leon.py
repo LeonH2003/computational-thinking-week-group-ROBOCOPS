a30886c9c264b8f8710dae3b14bff7d54753d088
jo-n-cas <joanasaraivacaseiro@gmail.com>
2025-09-01 10:51:16 +0200

    actually fixed spelling leon story

--- challenge_day3/leon.py
+++ challenge_day3/leon.py
@@ -2,7 +2,7 @@ def name():
     return "Leon"
 
 def act1():
-    return "Hi, I am Leon a knight travelling to destroy a dragon and prove my honor. I hope to win the heart of a mermaid in the process."
+    return "Hi, I am Leon a knight traveling to destroy a dragon and prove my honor. I hope to win the heart of a mermaid in the process."
 
 
 
@@ -10,4 +10,4 @@ def act2():
     return "Knight finds the dragon and challenges it to duel. The dragon breathes fire and the knight raises his shield to block the flames. The dragon then swipes at the knight with its claws, but the knight dodges and strikes back with his sword."
 
 def act3():
-    return "the knight is burned alive. Like a roasted marshmellow by that stupid dragon"
+    return "the knight is burned alive. Like a roasted marshmallow by that stupid dragon"

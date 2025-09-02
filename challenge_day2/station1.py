de6cce05c8e41d7e6356ad59aa5655b8a15ef2b6
jo-n-cas <joanasaraivacaseiro@gmail.com>
2025-09-02 10:37:17 +0200

    station 1

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -1,1 +1,9 @@
+import math
+
 def solution_station_1(num):
+    #fibonacci sequence
+    phi = (1 + math.sqrt(5)) / 2
+    for n in range(0, num + 1):
+        result = round(pow(phi, n) / math.sqrt(5))   
+    return result    
+

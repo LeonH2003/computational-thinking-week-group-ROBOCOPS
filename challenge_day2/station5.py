46d34bd45237746f8564b4514b434ce411082eba
LeonH2003 <leon.heemskerk@student.uva.nl>
2025-09-02 14:21:48 +0200

    station 5

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -1,1 +1,23 @@
-def solution_station_5(num):
+def solution_station_5(name):
+     # Define the learning teams and their members
+    lt1 = {"Daeho", "David", "Kaisa", "Oliver", "Sara", "Dan", "Ivar", "Lotte", "Riya", "Vassil", "Twan", "Ester", "Karolina", "Lena", "Margarita", "Anna", "Kien", "Klaudia", "Maliah", "Todd"}
+    lt2 = {"Oumaima", "Mathilde", "Marie", "Anita", "Ziyan", "Bernardo", "Eleanor", "Lorijn", "Maria", "Younes", "Yvan", "Henning", "Liangyu", "Maciej", "Toprak", "Chris", "GengXin", "Mingze", "Phoebe"}
+    lt3 = {"Betija", "Haider", "Kacper", "Sophie", "Amir", "Baltasar", "Isar", "Jelle", "Nicolas", "David", "Ipek", "Juan", "Marfa", "Maria", "Alissa", "Leopoldo", "Mies", "Jiaying", "Kaixin", "Mai", "Sem", "Tibbe"}
+    lt4 = {"Justus", "Julia", "Philip", "Uli", "Vanessa", "Anna", "Ekaterina", "Thessa", "Tongfei", "Yang", "Benedikt", "Jan", "Nadee", "Osjah", "Tim", "Eliana", "Joana", "Peilin", "Pija", "Wenhao"}
+    lt5 = {"Afua", "Cristina", "Greta", "Jace", "Laura", "Anna", "Bassant", "Ivan", "Juriaan", "Kiavash"}
+    lt6 = {"Keitaro", "Nohemi", "Norina", "Yifan", "Yinan", "Luo", "Nikola", "Olesya", "Sophie", "Tom"}
+
+    # Check which team the name belongs to
+    if name in lt1:
+        return 1
+    elif name in lt2:
+        return 2
+    elif name in lt3:
+        return 3
+    elif name in lt4:
+        return 4
+    elif name in lt5:
+        return 5
+    elif name in lt6:
+        return 6
+    
